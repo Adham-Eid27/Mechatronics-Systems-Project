3e7f2a622881038fac48fde898248c7ca84c5491
Adham-Eid27 <adhame900@gmail.com>
2024-12-12 15:59:46 +0200

    f

--- mechsystems_esp32/mechsystems_esp32.ino
+++ mechsystems_esp32/mechsystems_esp32.ino
@@ -1,73 +1,108 @@
-#include <WiFi.h>
+/*
+  Rui Santos
+  Complete project details at our blog.
+    - ESP32: https://RandomNerdTutorials.com/esp32-firebase-realtime-database/
+    - ESP8266: https://RandomNerdTutorials.com/esp8266-nodemcu-firebase-realtime-database/
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+  Based in the RTDB Basic Example by Firebase-ESP-Client library by mobizt
+  https://github.com/mobizt/Firebase-ESP-Client/blob/main/examples/RTDB/Basic/Basic.ino
+*/
+
+#include <Arduino.h>
+#if defined(ESP32)
+  #include <WiFi.h>
+#elif defined(ESP8266)
+  #include <ESP8266WiFi.h>
+#endif
 #include <Firebase_ESP_Client.h>
+
+//Provide the token generation process info.
 #include "addons/TokenHelper.h"
+//Provide the RTDB payload printing info and other helper functions.
 #include "addons/RTDBHelper.h"
 
+// Insert your network credentials
 #define WIFI_SSID "AASTGUEST"
 #define WIFI_PASSWORD ""
+
+// Insert Firebase project API Key
 #define API_KEY "AIzaSyAi7m5RWfiDAvIHr3786ryhX-UHvTIXEPY"
-#define DATABASE_URL "https://mechasystem-7394a-default-rtdb.europe-west1.firebasedatabase.app/"
 
-#define LDR_PIN 36
+// Insert RTDB URLefine the RTDB URL */
+#define DATABASE_URL "https://mechasystem-7394a-default-rtdb.europe-west1.firebasedatabase.app/" 
 
+//Define Firebase Data object
 FirebaseData fbdo;
+
 FirebaseAuth auth;
 FirebaseConfig config;
 
 unsigned long sendDataPrevMillis = 0;
+int count = 0;
 bool signupOK = false;
-int ldrData =0;
-float voltage =0.0;
 
-void setup() {
-Serial.begin(115200);
-WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-Serial.print ("CONNECTING  TO WI-FI");
-while(WiFi.status() != WL_CONNECTED){
-  Serial.print("..."); delay(300);
+void setup(){
+  Serial.begin(115200);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  /* Assign the api key (required) */
+  config.api_key = API_KEY;
+
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+
+  /* Sign up */
+  if (Firebase.signUp(&config, &auth, "", "")){
+    Serial.println("ok");
+    signupOK = true;
+  }
+  else{
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+  
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
 }
-Serial.println();
-Serial.print("Connected with IP: ");
-Serial.println(WiFi.localIP());
-Serial.println();
 
-config.api_key=API_KEY;
-config.database_url=DATABASE_URL;
-if(Firebase.signUp(&config, &auth, "", "")){
-  Serial.println("SignUP OK");
-signupOK = true;
-}else{
-  Serial.printf("%s\n", config.signer.signupError.message.c_str());
-}
-config.token_status_callback = tokenStatusCallback;
-Firebase.begin(&config, &auth);
-Firebase.reconnectWiFi(true);
-}
-void loop() {
+void loop(){
 
-  ldrData = random();
-  voltage = random();
-
-  if(Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 1000 || sendDataPrevMillis ==0)){
-    sendDataPrevMillis= millis();
-    ldrData = analogRead(LDR_PIN);
-    voltage = (float)analogReadMilliVolts(LDR_PIN)/1000;
-    if(Firebase.RTDB.setInt(&fbdo, "Sensor/ldr_data", ldrData)){
-      Serial.println();
-      Serial.print(ldrData);
-      Serial.print(" - successfully saved to: " + fbdo.dataPath());
-      Serial.println(" (" + fbdo.dataType() + ") ");
-    }else{
-      Serial.println("FAILED: " + fbdo.errorReason());
+  Serial.println("I'm working");
+  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)){
+    sendDataPrevMillis = millis();
+    // Write an Int number on the database path test/int
+    if (Firebase.RTDB.setInt(&fbdo, "test/int", count)){
+      Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
     }
-    if(Firebase.RTDB.setFloat(&fbdo, "Sensor/Voltage", voltage)){
-      Serial.println();
-      Serial.print(voltage);
-      Serial.print("- succesfully saved to: " + fbdo.dataPath());
-      Serial.println(" (" + fbdo.dataType() + ")");
-    }else{
-      Serial.println("FAILED: " + fbdo.errorReason());
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+    count++;
+    
+    // Write an Float number on the database path test/float
+    if (Firebase.RTDB.setFloat(&fbdo, "test/float", 0.01 + random(0,100))){
+      Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
     }
   }
-  
 }
