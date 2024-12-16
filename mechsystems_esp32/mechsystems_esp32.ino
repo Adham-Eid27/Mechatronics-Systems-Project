9283b8e8c3f6d96d226e5b348dffa194b296024b
Adham-Eid27 <adhame900@gmail.com>
2024-12-16 08:41:59 +0200

    4 sensor readings verify succesfully

--- mechsystems_esp32/mechsystems_esp32.ino
+++ mechsystems_esp32/mechsystems_esp32.ino
@@ -2,13 +2,16 @@
 #include <Firebase_ESP_Client.h>
 #include "addons/TokenHelper.h"
 #include "addons/RTDBHelper.h"
+#include "DHT.h"
 
 #define WIFI_SSID "Esp32test"
 #define WIFI_PASSWORD "yess1234"
 #define API_KEY "AIzaSyAi7m5RWfiDAvIHr3786ryhX-UHvTIXEPY"
 #define DATABASE_URL "https://mechasystem-7394a-default-rtdb.europe-west1.firebasedatabase.app/"
 
-#define LDR_PIN 36
+int LDR_PIN=34;
+int DHTpin=33;
+int Line_follower=32;
 
 FirebaseData fbdo;
 FirebaseAuth auth;
@@ -17,10 +20,18 @@ FirebaseConfig config;
 unsigned long sendDataPrevMillis = 0;
 bool signupOK = false;
 int ldrData =0;
-float voltage =0.0;
+int line =0;
+
+float temp;
+float humid;
+
+DHT dht(DHTpin, DHT11);
 
 void setup() {
 Serial.begin(115200);
+
+dht.begin();
+
 WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 Serial.print ("CONNECTING  TO WI-FI");
 while(WiFi.status() != WL_CONNECTED){
@@ -47,28 +58,59 @@ void loop() {
 
   if(Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 1000 || sendDataPrevMillis ==0)){
     sendDataPrevMillis= millis();
+    //LDR
     ldrData = analogRead(LDR_PIN);
-    ldrData = random(200);
-    voltage = (float)analogReadMilliVolts(LDR_PIN)/1000;
-    voltage = random(200);
+    //DHT11
+    temp = dht.readTemperature();
+    humid = dht.readHumidity();
+    //Line Follower Analog input
+    line = analogRead(Line_follower);
     
-    if(Firebase.RTDB.setInt(&fbdo, "Sensor/ldr_data", ldrData)){
+
+    //LDR
+    if(Firebase.RTDB.setInt(&fbdo, "Sensor/LDR", ldrData)){
       Serial.println();
+      ldrData = random(200);
       Serial.print(ldrData);
       Serial.print(" - successfully saved to: " + fbdo.dataPath());
       Serial.println(" (" + fbdo.dataType() + ") ");
     }else{
       Serial.println("FAILED: " + fbdo.errorReason());
     }
-    if(Firebase.RTDB.setFloat(&fbdo, "Sensor/Voltage", voltage)){
+
+    //DHT11 temperature
+    if(Firebase.RTDB.setFloat(&fbdo, "Sensor/DHT:Temperature", temp)){
       Serial.println();
-      Serial.print(voltage);
-      voltage = random(200);
+      temp = random(200);
+      Serial.print(temp);
       Serial.print("- succesfully saved to: " + fbdo.dataPath());
       Serial.println(" (" + fbdo.dataType() + ")");
     }else{
       Serial.println("FAILED: " + fbdo.errorReason());
     }
+
+    //DHT11 humid
+    if(Firebase.RTDB.setFloat(&fbdo, "Sensor/DHT:Humidity", humid)){
+      Serial.println();
+      humid = random(200);
+      Serial.print(humid);
+      Serial.print("- succesfully saved to: " + fbdo.dataPath());
+      Serial.println(" (" + fbdo.dataType() + ")");
+    }else{
+      Serial.println("FAILED: " + fbdo.errorReason());
+    }
+
+    //Line follower
+    if(Firebase.RTDB.setFloat(&fbdo, "Sensor/Line Follower", Line_follower)){
+      Serial.println();
+      Line_follower = random(200);
+      Serial.print(Line_follower);
+      Serial.print("- succesfully saved to: " + fbdo.dataPath());
+      Serial.println(" (" + fbdo.dataType() + ")");
+    }else{
+      Serial.println("FAILED: " + fbdo.errorReason());
+    }
+
   }
   
 }
